bfdf4f64538f48eab80b588c0dce9727186d1cb2
zeitseeing <zeitseeing@protonmail.com>
2022-11-09 15:51:37 +0100

    Make function asynchronous and add error messages

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ require('dotenv').config();
 const url = "https://zeroxbob.com/registrations";
 const email_address = process.env.EMAIL_ADDRESS
 
-function registerContract(contract_address) {
-  axios
+async function registerContract(contract_address) {
+  await axios
     .post(url, {
       address: contract_address,
       email: email_address,
     })
     .then(function (response) {
-      console.log(response);
+      console.log('zeroxbob.com success')
+      console.log(response.data);
     })
     .catch(function (error) {
-      console.log(error);
+      console.log('zeroxbob.com error')
+      console.log(error.response.data);
     });
 }
 
